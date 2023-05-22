f5cc16726bd05adb83cfd36d71eaf035a2e3f266
zhifeng huang <2292614355@qq.com>
2023-05-22 11:51:55 +0000

    work5

---
+++ work5.py
@@ -0,0 +1,13 @@
+#列表[1，2，3，4，5]，请使用map()函数输出[1,4,9,16,25],并使用列表推导式提取出大于10的数，最终输出[16,25]
+lis = [1,2,3,4,5]
+
+def fn(x):
+    return x**2
+
+res = map(fn, lis)#使用map()函数输出
+
+print(res)
+
+res = [i for i in res if i > 10]#提取大于10的数
+
+print(res)
